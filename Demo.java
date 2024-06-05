a4ebbc4e3663a14ed1ddb7b460f264c60dfc989d
Saroj Budhathoki <budhathokisaroj827@gamil.com>
2024-06-05 08:37:46 +0545

    change nothing

--- Demo.java
+++ Demo.java
@@ -6,7 +6,14 @@ public class Demo {
 
     public static void main(String[] args) {
         
-        System.out.println("hello world");
+        // add two numbers 
+        int num = 2;
+        int num2 = 4;
+        int sum = num + num2;
+        System.out.println("Sum of " + num + " and " + num2 + " is " + sum);
+
+
+
     }
     
 }
